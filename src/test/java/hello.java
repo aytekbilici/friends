d4005d6dfbd5211e66a59156e35bd73b347f4c6b
Serkan Demir <dmrsrknn@gmail.com>
2020-11-25 18:25:09 +0100

    second commit

---
+++ src/test/java/hello.java
@@ -0,0 +1,28 @@
+public class hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello friends");
+
+
+        System.out.println("Aytekin abi ");
+
+        System.out.println("Efendi abi ");
+
+        System.out.println("Ferhat abi ");
+
+        System.out.println("Hasan abi ");
+
+        System.out.println("Mehmet abi ");
+
+        System.out.println("Murat abi");
+
+        System.out.println("Naci abi ");
+
+        System.out.println("Nuri abi ");
+
+        System.out.println("Zafer abi ");
+
+
+    }
+}
